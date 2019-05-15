bc3df87ecf59c250c4ac652342d8d15af0e8a710
Zing Yang <zing9264@gmail.com>
2019-05-16 03:23:42 +0800

    part2 btn

--- app.js
+++ app.js
@@ -3,8 +3,26 @@
 //
 // See HW4 writeup for more hints and details.
 class App {
-  constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
-  }
-  // TODO(you): Add methods as necessary.
+    constructor() {
+        // TODO(you): Implement the constructor and add fields as necessary.
+
+        this._onSubmit=this._onSubmit.bind(this);
+
+        const menuElement = document.querySelector('#menu');
+        this.menuScreen = new MenuScreen(menuElement);
+
+        const form = document.querySelector('form');
+        form.addEventListener('submit', this._onSubmit);
+
+        const musicElement = document.querySelector('#musicbox');
+        this.musicScreen = new MusicScreen(musicElement);
+
+    }
+
+    // TODO(you): Add methods as necessary.
+
+    _onSubmit(event) {
+        event.preventDefault();
+        this.musicScreen.show();
+    }
 }
